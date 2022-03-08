6f8dad7ccebc6824c003a9af329687d61f17f004
Matthew Pearsall <matthew.pearsall@ogl.co.uk>
2022-03-08 15:29:24 +0000

    Update Spring Boot; Allow all origins

--- src/main/java/com/ogl/devtest/DevtestApplication.java
+++ src/main/java/com/ogl/devtest/DevtestApplication.java
@@ -2,6 +2,9 @@ package com.ogl.devtest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class DevtestApplication {
@@ -10,4 +13,13 @@ public class DevtestApplication {
     SpringApplication.run(DevtestApplication.class, args);
   }
 
+  @Bean
+  public WebMvcConfigurer webConfiguration() {
+    return new WebMvcConfigurer() {
+      @Override
+      public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedMethods("*");
+      }
+    };
+  }
 }
